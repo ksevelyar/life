c079759bdc9a8a297859456d8816fde2b487284e
Sergey Zubkov <ksevelyar@protonmail.com>
2023-07-13 20:04:31 +0300

    change cell size to 3px

--- life.js
+++ life.js
@@ -2,17 +2,14 @@ import init, { Universe, Cell } from './docs/life_wasm.js'
 
 async function run() {
   const canvas = document.querySelector('.universe')
-  canvas.height = window.innerHeight
-  canvas.width = window.innerWidth
-
   const wasm = await init()
  
-  const cell_size = 5
+  const cell_size = 3
   const height = Math.floor(window.innerHeight / (cell_size + 1))
   const width = Math.floor(window.innerWidth / (cell_size + 1))
 
-  canvas.height = height * 6 + 1
-  canvas.width = width * 6 + 1
+  canvas.height = height * (cell_size + 1) + 1
+  canvas.width = width * (cell_size + 1) + 1
 
   const universe = Universe.new(width, height)
 
@@ -29,13 +26,32 @@ async function run() {
 
     ctx.beginPath()
 
+    // Alive cells.
+    ctx.fillStyle = alive_color
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++) {
         const idx = getIndex(row, col)
+        if (cells[idx] !== Cell.Alive) {
+          continue
+        }
 
-        ctx.fillStyle = cells[idx] === Cell.Dead
-          ? dead_color
-          : alive_color
+        ctx.fillRect(
+          col * (cell_size + 1) + 1,
+          row * (cell_size + 1) + 1,
+          cell_size,
+          cell_size
+        )
+      }
+    }
+
+    // Dead cells.
+    ctx.fillStyle = dead_color
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col)
+        if (cells[idx] !== Cell.Dead) {
+          continue
+        }
 
         ctx.fillRect(
           col * (cell_size + 1) + 1,
